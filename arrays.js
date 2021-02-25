4a97bb51a337c462742f6c1d196aee0fd04b7502
Bryan Imbaquingo <bryan_3871@hotmail.com>
2021-02-25 13:36:41 -0500

    inserFirst, delItem, delFirst

--- arrays.js
+++ arrays.js
@@ -16,13 +16,65 @@ class MyArray {
     this.length++;
     return this.data;
   }
+
+  pop() {
+    // guardamos el ultimo elemento
+    const lastItem = this.data[this.length - 1];
+    // eliminamos el último elemento
+    delete this.data[this.length - 1];
+    // borra la posicion
+    this.length--;
+    return lastItem;
+  }
+
+  delItem(index) {
+    const item = this.data[index];
+    this.shiftIndex(index);
+    return item;
+  }
+
+  shiftIndex(index) {
+    for (var i = index; i < this.length - 1; i++) {
+      // hace el cambio de indices
+      this.data[i] = this.data[i + 1];
+    }
+    // borra el contenido en el index
+    delete this.data[this.length - 1];
+    // decrementamos en 1 el index
+    this.length--;
+  }
+
+  inserFirst(item) {
+    for (var i = this.length; i > 0; i--) {
+      // hace el cambio de indices
+      this.data[i] = this.data[i - 1];
+    }
+    this.data[0] = item;
+    this.length++;
+    return this.data;
+  }
+
+  delFirst() {
+    this.delItem(0);
+  }
 }
 
 const elArray = new MyArray();
 
-console.table(elArray);
 elArray.push("Carlos");
 console.table(elArray);
 elArray.push("Marlon");
 console.table(elArray);
-console.log(elArray.get(1));
+elArray.delFirst();
+console.table(elArray);
+elArray.push("Chente");
+console.table(elArray);
+elArray.push("Main");
+console.table(elArray);
+elArray.pop();
+console.table(elArray);
+elArray.delItem(1);
+console.table(elArray);
+
+elArray.inserFirst("hola");
+console.table(elArray);
